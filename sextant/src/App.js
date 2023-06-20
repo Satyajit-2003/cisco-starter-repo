0207c1ef908b3794820ea8980c06c71f002f573d
Satyajit Satapathy <satyajit1470@gmail.com>
2023-06-20 23:13:14 +0530

    Task 4: Done

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -2,17 +2,21 @@ import './App.css';
 import Banner from './Banner.js'
 import Exhibit from './Exhibit.js'
 import AddressDisplay from './AddressDisplay.js'
+import PylonConnector from './PylonConnector';
 
 function App() {
   
   return (
     <div className="App">
-                <Banner bannerText="Sextant" />
+                <Banner bannerText="IP Adress finder" />
                 <Exhibit name="Public IPv4 Address">
                     <AddressDisplay url='https://api.ipify.org?format=json' />
                 </Exhibit>
                 <Exhibit name="Public IPv6 Address">
                     <AddressDisplay url='https://api64.ipify.org?format=json' />
+                </Exhibit>
+                <Exhibit name="Latency">
+                    <PylonConnector />
                 </Exhibit>
             </div>
   );
